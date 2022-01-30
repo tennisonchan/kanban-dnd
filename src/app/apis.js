3685056878377ac70f9235ee9be02a931f8b588a
Tennison Chan <tennison.chan+gh@gmail.com>
2022-01-30 02:21:07 -0800

    Add user slice, user hook

--- src/app/apis.js
+++ src/app/apis.js
@@ -1,52 +1,45 @@
 import axios from "axios";
 
-const PORT = 3001;
+const PORT = 3000;
 const BASE_URL = `http://localhost:${PORT}`;
 const API_URL = `${BASE_URL}/api`;
-const config = {
-  headers: {
-    "Content-Type": "application/json;charset=utf-8",
-    "Access-Control-Allow-Origin": "*",
-  },
-};
 
-export const getProjects = () => axios.get(`${API_URL}/projects`, config);
+export const getProjects = () => axios.get(`${API_URL}/projects`);
 
 export const getProject = (projectId) =>
-  axios.get(`${API_URL}/projects/${projectId}`, config);
+  axios.get(`${API_URL}/projects/${projectId}`);
 
 export const postProject = (payload) =>
-  axios.post(`${API_URL}/project/`, payload, config);
+  axios.post(`${API_URL}/project/`, payload);
 
 export const patchProject = (payload) =>
-  axios.patch(`${API_URL}/project/`, payload, config);
+  axios.patch(`${API_URL}/project/`, payload);
 
 export const deleteProject = (payload) =>
-  axios.delete(`${API_URL}/project/`, payload, config);
+  axios.delete(`${API_URL}/project/`, payload);
 
-export const postColumn = (payload) =>
-  axios.post(`${API_URL}/column`, payload, config);
+export const postColumn = (payload) => axios.post(`${API_URL}/column`, payload);
 
 export const patchColumn = (payload) =>
-  axios.patch(`${API_URL}/column`, payload, config);
+  axios.patch(`${API_URL}/column`, payload);
 
 export const deleteColumn = (columnId) =>
-  axios.delete(`${API_URL}/columns/${columnId}`, config);
+  axios.delete(`${API_URL}/columns/${columnId}`);
 
 export const postColumnsReorder = (payload) =>
-  axios.post(`${API_URL}/columns/reorder`, payload, config);
+  axios.post(`${API_URL}/columns/reorder`, payload);
 
-export const postNote = (payload) =>
-  axios.post(`${API_URL}/note`, payload, config);
+export const postNote = (payload) => axios.post(`${API_URL}/note`, payload);
 
-export const patchNote = (payload) =>
-  axios.patch(`${API_URL}/note`, payload, config);
+export const patchNote = (payload) => axios.patch(`${API_URL}/note`, payload);
 
 export const deleteNote = ({ noteId, columnId }) =>
-  axios.delete(`${API_URL}/columns/${columnId}/notes/${noteId}`, config);
+  axios.delete(`${API_URL}/columns/${columnId}/notes/${noteId}`);
 
 export const postNoteArchive = (payload) =>
-  axios.post(`${API_URL}/note/archive`, payload, config);
+  axios.post(`${API_URL}/note/archive`, payload);
 
 export const postNotesReorder = (payload) =>
-  axios.post(`${API_URL}/notes/reorder`, payload, config);
+  axios.post(`${API_URL}/notes/reorder`, payload);
+
+export const postJWT = (payload) => axios.post(`${API_URL}/jwt`, payload);
