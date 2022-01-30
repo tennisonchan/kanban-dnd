fbd8fd491eecffa606cb1cbb27d5bed79b27e6b5
Tennison Chan <tennison.chan+gh@gmail.com>
2022-01-30 01:58:54 -0800

    Add User model

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new mongoose.Schema(
+  {
+    terraAddress: { type: String, required: true },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+UserSchema.statics.findOneOrCreate = function (condition, callback) {
+  this.findOne(condition, (err, result) => {
+    console.log("findOne", result);
+    return !!result
+      ? callback(err, result)
+      : this.create(condition, (err, result) => {
+          console.log("create", result);
+          return callback(err, result);
+        });
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
